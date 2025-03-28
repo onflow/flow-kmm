2f145a8a50a279f69cc770844123c17a52fbe953
Lea Lobanov <lea.lobanov@icloud.com>
2025-03-28 21:26:29 +0300

    CICD on pull request

--- flow/src/commonMain/kotlin/org/onflow/flow/Utils.kt
+++ flow/src/commonMain/kotlin/org/onflow/flow/Utils.kt
@@ -1,8 +1,4 @@
 import com.google.common.io.BaseEncoding
-import com.google.protobuf.Timestamp
-import java.security.MessageDigest
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 fun ByteArray.bytesToHex(): String = BaseEncoding.base16().lowerCase().encode(this)
 
