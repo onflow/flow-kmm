36324159eb0de1543a13d3ca7b86415700a6b5bc
Lea Lobanov <lea.lobanov@icloud.com>
2025-03-28 14:29:05 +0300

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.1"
+    val defaultVersion = "0.0.2"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
