45a4c2ee54dcc4384b334f02105c7cecba3f1d3c
Lea Lobanov <44328396+lealobanov@users.noreply.github.com>
2025-05-26 18:04:12 +0900

    Add transient annotation (#24)
    * Add transient annotation
    
    * Add transient annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.7"
+    val defaultVersion = "0.0.8"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
