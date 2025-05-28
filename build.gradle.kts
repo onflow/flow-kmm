43860c748661a1426234ac4777dd95896afebfc2
Lea Lobanov <44328396+lealobanov@users.noreply.github.com>
2025-05-28 19:22:43 +0900

    Signer index debugging (#27)
    * Signer index debugging
    
    * Signer index debugging

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.10"
+    val defaultVersion = "0.0.11"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
