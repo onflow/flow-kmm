6d4066f4e143a08b0015d457f75b685826d798ac
Lea Lobanov <lea.lobanov@icloud.com>
2025-06-17 01:50:11 +0900

    Parsing Cadence resource type

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.16"
+    val defaultVersion = "0.0.17"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
