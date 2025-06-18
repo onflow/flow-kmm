31089ad4b64af44ca1f8fe54e313084183e5d174
Lea Lobanov <44328396+lealobanov@users.noreply.github.com>
2025-06-19 02:31:40 +0900

    Bump version 2 (#42)
    * Bump version
    
    * Bump version
    
    * Bump version
    
    * Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.19"
+    val defaultVersion = "0.0.20"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
