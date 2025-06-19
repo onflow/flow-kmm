377bc4b53955fdd35273112a7fee76acb45eba82
Lea Lobanov <lea.lobanov@icloud.com>
2025-06-19 09:16:50 +0900

    Wait for FINAL state

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
     }
 
     group = "org.onflow.flow"
-    val defaultVersion = "0.0.20"
+    val defaultVersion = "0.0.21"
     version = System.getenv("GITHUB_REF")?.split('/')?.last() ?: defaultVersion
 }
 
